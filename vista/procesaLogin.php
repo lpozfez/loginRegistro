a32a321309777eefac1cbb68942bfae4b37fc3e5
Lourdes <lpozfer614@g.educaand.es>
2022-09-30 12:45:39 +0200

    V.1.1 Mejora de procesaLogin

--- vista/procesaLogin.php
+++ vista/procesaLogin.php
@@ -7,49 +7,42 @@
     $rolMindundi='usuario';
     $admin='administrador';
     $user= explode("\n",$ficheroDatos);//array con cada línea del string
-    
-    foreach($user as $valor){
+    $encontrado=false;
+    $i=0;
+    while($encontrado==false && $i<count($user)){//Esto a fichero de datos
         //Por cada fila separamos los datos y los añadimos al array de ususarios $user
-        $usu=explode(";",$valor)[0];
-        $cont=explode(";",$valor)[1];
-        $rol=explode(";",$valor)[2];
-    
-        
-        echo 'Este es el usuario del formu'.$usuario;
-        echo 'Este es contraseña del formu'.$pass;
+        $usu=explode(";",$user[$i])[0];
+        $cont=explode(";",$user[$i])[1];
+        $rol=explode(";",$user[$i])[2];
 
+        
         if($usuario==$usu && $pass==$cont){
-            if($rol==$rolMindundi){
-                echo 'Bienvenido '.$usu.'<br>';
-                echo 'Contraseña: '.$cont.'<br>';
-                echo 'Rol: '.$rol.'<br>';
-                break;
-            }elseif($rol==$admin){
-                header("Location: registroUsu.html" );
-                break;
-            }
+            $encontrado=true;
         }else{
-            echo 'Usted no está registrado';
-            
+            $i++;
         }
-        echo 'Este es el usuario del array'.$usu;
-        echo 'Este es contraseña del formu'.$cont;
+        
         
     }
-
-
-
-
-
-
-
-
+  
     
-    /*for($i=0;$i<count($user);$i++){
-        $usu[$i]=explode(";",$user[$i]);
-        echo $usu[0];
-        echo $usu[1];
-        echo $usu[2];
-    }*/
+
+    if($encontrado){
+        
+        if($rol==$rolMindundi){
+            
+            echo 'Bienvenido '.$usu.'<br>';
+            echo 'Contraseña =>'.$cont.'<br>';
+            echo 'rol =>'.$rol.'<br>';
+            
+
+        }else{
+            var_dump ($rol);
+            //header("Location: registroUsu.html");
+        }
+    }else{
+        echo 'Usted no está registrado';
+    }
+
         
 ?>
